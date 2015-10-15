199aee78cb494169eb8b73fbb65de5ae40a5d436
Hans Fangohr osiris <fangohr@soton.ac.uk>
2015-10-15 09:02:07 +0100

    Use pytest fixture to deal with compilation

--- other/wrapping-cpp/swig/c++/test_mylib.py
+++ other/wrapping-cpp/swig/c++/test_mylib.py
@@ -1,14 +1,24 @@
 import os
-import mylib
-os.system('make all')
+
+import pytest
+
+@pytest.fixture
+def setup(request):
+    def teardown():
+        print("Running make clean")
+        os.system('make clean')
+        print("Completed finaliser")
+    request.addfinalizer(teardown)
+    os.system('make clean')
+    os.system('make all')
 
 
-def test_squared():
+def test_squared(setup):
+    import mylib
     assert 16. == mylib.squared(4) 
 
 
-def test_myfunction():
+def test_myfunction(setup):
+    import mylib
     assert 16. == mylib.myfunction(mylib.squared, 4) 
 
-
-os.system('make clean')

23d280e5c655a0f74e5230216b29db2b7fb08c27
Hans Fangohr osiris <fangohr@soton.ac.uk>
2015-10-15 08:52:20 +0100

    For these examples we can reasonable expect exact agreement
    for the limited architectures we intend to support (x86)

--- other/wrapping-cpp/swig/c++/test_mylib.py
+++ other/wrapping-cpp/swig/c++/test_mylib.py
@@ -4,11 +4,11 @@ os.system('make all')
 
 
 def test_squared():
-    assert mylib.squared(4) - 16.0 <= 10 ** -8
+    assert mylib.squared(4) == 16.0 
 
 
 def test_myfunction():
-    assert mylib.myfunction(mylib.squared, 4) - 16.0 <= 10 ** -8
+    assert mylib.myfunction(mylib.squared, 4) == 16.0 
 
 
 os.system('make clean')

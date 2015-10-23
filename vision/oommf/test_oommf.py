bf5a239397040e057a223b72ad78d82948ca1466
Ryan Pepper <ryan.pepper1@gmail.com>
2015-10-23 13:25:23 +0000

    wrote first test

---
+++ vision/oommf/test_oommf.py
@@ -0,0 +1,13 @@
+def test_oommf_sim():
+    import oommf
+    oommf.path = "/home/vagrant/oommf-python/oommf/oommf/"
+    Py = oommf.materials.permalloy
+    my_geometry = oommf.geometry.Cuboid((0,0,0), (30, 30, 100), unitlength=1e-9)
+    sim = oommf.Simulation(my_geometry, cellsize=5e-9, material=Py)
+    sim.m = [1, 1, 0]
+    assert str(sim) == 'Simulation: Py(Fe80Ni20). \n\tGeometry: Cuboid corner1 = (0, 0, 0), corner2 = (30, 30, 100). \n\t          Cells = [6, 6, 20], total=720.'
+    sim.advance_time(1e-9)
+    assert str(sim) == 'Simulation: Py(Fe80Ni20). \n\tGeometry: Cuboid corner1 = (0, 0, 0), corner2 = (30, 30, 100). \n\t          Cells = [6, 6, 20], total=720.\n\tCurrent t = 1e-09s'
+
+    
+

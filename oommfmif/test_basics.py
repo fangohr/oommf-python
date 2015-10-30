a9893fc562c9131fdaebaa842f587f415b7fdfda
Hans Fangohr Lubuntu 14.04 virt MBA <fangohr@soton.ac.uk>
2015-10-30 14:06:38 +0000

    Add second test.

--- oommfmif/test_basics.py
+++ oommfmif/test_basics.py
@@ -1,5 +1,9 @@
 import oommfmif as o
 
 
-def test_get_oommf_version():
+def test_get_oommf_version_return_type():
     assert isinstance(o.get_version(), str)
+
+
+def test_get_oommf_version():
+    assert o.get_version()[0:4] == "1.2."

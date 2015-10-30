685d925ff987412cb828f68f9fb75fa8ae4a2dac
Ryan Pepper <rp20g15@soton.ac.uk>
2015-10-30 20:36:23 +0000

    Copied over oommf part of Finmag tests repository

---
+++ utils/__init__.py
@@ -0,0 +1,8 @@
+# oommf-python
+# Copyright (C) 2015 University of Southampton
+#
+# CONTACT: h.fangohr@soton.ac.uk
+#
+# AUTHOR(S) OF THIS FILE: Dmitri Chernyshenko (d.chernyshenko@soton.ac.uk)
+
+from init import *

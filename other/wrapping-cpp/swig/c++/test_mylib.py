145cf9e539d44b3996a4ab916edb7873fa5090f0
Hans <fangohr@soton.ac.uk>
2015-10-30 16:35:26 +0000

    Allow file to be called from other directories

--- other/wrapping-cpp/swig/c++/test_mylib.py
+++ other/wrapping-cpp/swig/c++/test_mylib.py
@@ -2,15 +2,27 @@ import os
 
 import pytest
 
+# Need to call Makefile in directory where this test file is
+def call_make(target):
+    # where is this file
+    this_file = os.path.realpath(__file__)
+    this_dir = os.path.split(this_file)[0]
+    cd_command = "cd {}".format(this_dir)
+    make_command = "make {}".format(target)
+    command = '{}; {}'.format(cd_command, make_command)
+    print("About to execute: '{}'".format(command))
+    os.system(command)
+
+
 @pytest.fixture
 def setup(request):
     def teardown():
         print("Running make clean")
-        os.system('make clean')
+        call_make('clean')
         print("Completed finaliser")
     request.addfinalizer(teardown)
-    os.system('make clean')
-    os.system('make all')
+    call_make('clean')
+    call_make('all')
 
 
 def test_squared(setup):

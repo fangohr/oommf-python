56592b10e25cd1f2cf8d122df389268ab24b3729
Hans Fangohr Lubuntu 14.04 virt MBA <fangohr@soton.ac.uk>
2015-10-30 14:51:25 +0000

    refactor and use OOMMF_PATH environment variable to locate oommf.tcl

--- oommfmif/__init__.py
+++ oommfmif/__init__.py
@@ -1,13 +1,31 @@
+import os
 import subprocess
 
+# Environment variable OOMMF_PATH should point to the directory which
+# contains 'oommf.tcl'
+oommf_path = os.environ['OOMMF_PATH']
 
-def get_version():
-    p = subprocess.Popen("~/git/oommf/oommf/oommf.tcl +version", shell=True,
-                         stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+
+def call_oommf(argstring):
+    """Convenience function to call OOMMF: Typicallusage
+
+    p = call_oommf("+version")
     p.wait()
     stdout, stderr = p.stdout.read(), p.stderr.read()
 
-    # version is returned in stderr
-    print(stderr.split()[0:2])
+    """
+
+    p = subprocess.Popen(os.path.join(oommf_path, 'oommf.tcl') +
+                         ' ' + argstring,
+                         shell=True, stderr=subprocess.PIPE,
+                         stdout=subprocess.PIPE)
+    return p
+
+
+def get_version():
+    """Return OOMMF version as string, something like 1.2.0.5"""
+    p = call_oommf('+version')
+    p.wait()
+    stderr = p.stderr.read()     # version is returned in stderr
     s_oommftcl, versionstring = stderr.split()[0:2]
     return versionstring

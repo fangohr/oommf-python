66396062d442096ad316b0c66d34b996981b5359
Hans <fangohr@soton.ac.uk>
2015-10-30 20:32:37 +0000

    made oommf sample run smaller to be faster as a test

--- oommfmif/test_basics.py
+++ oommfmif/test_basics.py
@@ -1,3 +1,5 @@
+import os
+
 import oommfmif as o
 
 
@@ -16,14 +18,12 @@ def test_get_oommf_path():
 def test_run_oommf_simulation(tmpdir):
 
     bigbar_mif_1 = """# MIF 2.1
-# Sample problem description for muMAG Standard Problem #1
-set pi [expr 4*atan(1.0)]
-set mu0 [expr 4*$pi*1e-7]
+# Tiny test problem, OOMMF-Python
 
 Specify Oxs_BoxAtlas:atlas {
-  xrange {0 30e-9}
-  yrange {0 30e-9}
-  zrange {0 100e-9}
+  xrange {0 5e-9}
+  yrange {0 5e-9}
+  zrange {0 10e-9}
 }
 
 Specify Oxs_RectangularMesh:mesh {
@@ -33,11 +33,6 @@ Specify Oxs_RectangularMesh:mesh {
 
 Specify Oxs_UniformExchange {A  13e-12}
 
-
-Specify Oxs_Demag {}
-
-#Specify Oxs_UZeeman "Hrange { { 0.5e6 0 0 0.5e6 0 0 0 } }"
-
 Specify Oxs_EulerEvolve {
   alpha 0.5
   start_dm 0.0001
@@ -46,6 +41,7 @@ Specify Oxs_EulerEvolve {
   relative_step_error 0.02
 }
 
+Specify Oxs_Demag {}
 
 Specify Oxs_TimeDriver {
  basename bigbar
@@ -64,15 +60,26 @@ Specify Oxs_TimeDriver {
 
 Destination archive mmArchive
 
-Schedule DataTable archive Step 10
-Schedule Oxs_TimeDriver::Magnetization archive Stage 500
+Schedule DataTable archive Step 1
+Schedule Oxs_TimeDriver::Magnetization archive Stage 1
 """
 
-    import os.path
+    # write config file into tmp directory
     open(os.path.join(str(tmpdir), 'bigbar.mif'), 'w').write(bigbar_mif_1)
 
+    # call ommff
     process = o.call_oommf('boxsi bigbar.mif', workdir=tmpdir)
+
+    # wait for oommf to complete
     process.wait()
+
+    # read stdout and stderr, so we can see those in case of error
+    # via "py.test -l"
+    stdout, stderr = process.stdout.read(), process.stderr.read()
+    print("stdout = {}".format(stdout))
+    print("stderr = {}".format(stderr))
+
+    # assert output file has been created
     assert os.path.exists(os.path.join(str(tmpdir),
                           'bigbar-Oxs_TimeDriver-Magnetization-00-0000002.omf')
                           )

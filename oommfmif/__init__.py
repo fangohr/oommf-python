139b5f284a68513e3de9a739258f43052e1e21d5
Hans Fangohr Lubuntu 14.04 virt MBA <fangohr@soton.ac.uk>
2015-10-30 14:31:40 +0000

    First non-trivial implementation of getting the version from OOMMF

--- oommfmif/__init__.py
+++ oommfmif/__init__.py
@@ -1,2 +1,13 @@
+import subprocess
+
+
 def get_version():
-    return "1.2.0.5"
+    p = subprocess.Popen("~/git/oommf/oommf/oommf.tcl +version", shell=True,
+                         stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    p.wait()
+    stdout, stderr = p.stdout.read(), p.stderr.read()
+
+    # version is returned in stderr
+    print(stderr.split()[0:2])
+    s_oommftcl, versionstring = stderr.split()[0:2]
+    return versionstring

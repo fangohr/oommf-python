670525727bef61dcef176ab43736305ef9e34bb9
Hans Fangohr <fangohr@soton.ac.uk>
2015-11-09 20:05:06 +0100

    more testing implemented

--- utils/test_ovf.py
+++ utils/test_ovf.py
@@ -1,4 +1,12 @@
 import os
+import sys
+
+import pytest
+
+
+def test_python_is_2():
+    print("ovf2.py is not yet Python 3 compatible")
+    assert sys.version_info[0:2] == (2, 7)
 
 
 def test_can_import():
@@ -8,8 +16,8 @@ def test_can_import():
     lattice
 
 
-def test_example_docstring_write(tmpdir):
-    from ovf import OVFFile, OVF10, OVF20
+@pytest.fixture
+def field1():
     from lattice import FieldLattice
 
     # Create the data
@@ -18,13 +26,17 @@ def test_example_docstring_write(tmpdir):
     def setter_function(position):
         return [1, 0, 0]
     fl.set(setter_function)
+    return fl
 
+
+def test_example_docstring_write_ovff20(tmpdir, field1):
+    from ovf import OVFFile, OVF20
+    fl = field1
     # Save it to file, with OVF2 format
     ovf = OVFFile()
     ovf.new(fl, version=OVF20, data_type="binary8")
     path = os.path.join(str(tmpdir), "newfile.ovf")
     ovf.write(path)
-    ovf.write("test.ovf")
     assert os.path.exists(path)
     filecontent = open(path).read()
     assert filecontent.startswith("""# OOMMF OVF 2.0
@@ -32,12 +44,18 @@ def test_example_docstring_write(tmpdir):
 # Begin: Segment
 # Begin: Header
 # Title: Title""")
+    # for reading test
+    ovf.write("tmp_test_ovf2.ovf")
 
+
+def test_example_docstring_write_ovff10(tmpdir, field1):
+    from ovf import OVFFile, OVF10
+    fl = field1
     ovf = OVFFile()
     ovf.new(fl, version=OVF10, data_type="binary8")
     path = os.path.join(str(tmpdir), "newfile.ovf")
     ovf.write(path)
-    ovf.write("test.ovf")
+    ovf.write("tmp_test_ovf1.ovf")
     assert os.path.exists(path)
     filecontent = open(path).read()
     assert filecontent.startswith("""# OOMMF: rectangular mesh v1.0
@@ -47,3 +65,13 @@ def test_example_docstring_write(tmpdir):
 # Title: Title
 # meshtype: rectangular
 # meshunit: 1.0""")
+
+
+def test_example_docstring_read_ovf_file():
+    from ovf import OVFFile
+    ovf_file = OVFFile("tmp_test_ovf1.ovf")
+    fl = ovf_file.get_field()
+    assert fl is not None
+    # fl is a FieldLattice object, see module lattice.py
+    # fl.lattice is a Lattice object, describing the mesh (lattice.py)
+    #  fl.field_data is the numpy array containing the data

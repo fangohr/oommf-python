a17be3989c9bb3f3a4df1c615e757ca7fc7271e8
mb4e10 <mb4e10@soton.ac.uk>
2015-11-27 13:40:04 +0000

    Add creating mesh string.

--- pyoommf/mesh.py
+++ pyoommf/mesh.py
@@ -18,8 +18,17 @@ class Mesh(object):
         atlas_mif += '}\n\n'
         return atlas_mif
 
-mesh = Mesh(50e-9, 50e-9, 50e-9, 5-9, 5e-9, 5e-9)
+    def mesh_mif(self):
+        mesh_mif = 'Specify Oxs_RecatngularMesh:mesh {\n'
+        mesh_mif += '\tcellsize {%2e %2e %2e}\n' % (self.dx, self.dy, self.dz)
+        mesh_mif += '\tatlas :atlas\n'
+        mesh_mif += '}\n\n'
+        return mesh_mif
 
-f = open('mesh.mif', 'w')
-f.write(mesh.atlas_mif())
-f.close()
+if __name__ == '__main__':
+    mesh = Mesh(50e-9, 50e-9, 50e-9, 5-9, 5e-9, 5e-9)
+
+    f = open('mesh.mif', 'w')
+    f.write(mesh.atlas_mif())
+    f.write(mesh.mesh_mif())
+    f.close()

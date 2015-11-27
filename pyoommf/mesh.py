7cc70bce8a7c155243d80329463ee00918224ad8
mb4e10 <mb4e10@soton.ac.uk>
2015-11-27 12:48:12 +0000

    Add mesh class.

---
+++ pyoommf/mesh.py
@@ -0,0 +1,11 @@
+class Mesh(object):
+    def __init__(self, lx, ly, lz, dx, dy, dz):
+        self.lx = lx
+        self.ly = ly
+        self.lz = lz
+        self.dx = dx
+        self.dy = dy
+        self.dz = dz
+
+    def mesh_info(self):
+        return (lx, ly, lz, dx, dy, dz)

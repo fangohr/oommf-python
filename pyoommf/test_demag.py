041ca4a738a2cab28fdda0bc205769f30d2233db
mb4e10 <mb4e10@soton.ac.uk>
2015-11-27 16:13:30 +0000

    Extend demag test.

--- pyoommf/test_demag.py
+++ pyoommf/test_demag.py
@@ -1,6 +1,13 @@
-import demag
+from demag import Demag
 
-def test_demag():
-    a = demag.Demag()
-    assert 'Specify Oxs_Demag {}' in a.get_mif() 
- 
+def test_demag_mif():
+    demag = Demag()
+    mif_string = demag.get_mif()
+    assert 'Specify Oxs_Demag {}' in mif_string
+    
+def test_demag_formatting():
+    demag = Demag()
+    mif_string = demag.get_mif()
+    assert mif_string[0] == 'S'
+    assert mif_string[-1] == '\n'
+    assert mif_string[-2] == '\n'

12b5cc6a5b88999416238fb4305da7afdf79c374
mb4e10 <mb4e10@soton.ac.uk>
2015-11-27 13:35:40 +0000

    Add creating atlas mesh string to mesh.

--- pyoommf/mesh.py
+++ pyoommf/mesh.py
@@ -9,3 +9,17 @@ class Mesh(object):
 
     def mesh_info(self):
         return (self.lx, self.ly, self.lz, self.dx, self.dy, self.dz)
+
+    def atlas_mif(self):
+        atlas_mif = 'Specify Oxs_BoxAtlas:atlas {\n'
+        atlas_mif += '\t xrange {0 %2e}\n' % self.lx
+        atlas_mif += '\t yrange {0 %2e}\n' % self.ly
+        atlas_mif += '\t zrange {0 %2e}\n' % self.lz
+        atlas_mif += '}\n\n'
+        return atlas_mif
+
+mesh = Mesh(50e-9, 50e-9, 50e-9, 5-9, 5e-9, 5e-9)
+
+f = open('mesh.mif', 'w')
+f.write(mesh.atlas_mif())
+f.close()

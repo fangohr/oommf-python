e03c97c33f46342f702693e4691f943187af526a
mb4e10 <mb4e10@soton.ac.uk>
2015-11-27 12:54:17 +0000

    Add simulation class.

---
+++ pyoommf/sim.py
@@ -0,0 +1,24 @@
+class Sim(object):
+    def __init__(mesh, Ms):
+        self.mesh = mesh
+        self.Ms = Ms
+        self.energies = []
+
+    def add_exchange(self, A):
+        self.energies.append(('Exchange', A))
+
+    def add_demag(self):
+        self.energies.append(('Demag', None))
+
+    def add_zeeman(self, H):
+        self.energies.append(('Zeeman', H))
+
+    def set_m(self, m_init):
+        self.m_init = m_init
+
+    def create_mif(self):
+        pass
+
+    def run_until(self, t_sim):
+        pass
+

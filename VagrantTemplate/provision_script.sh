88a7396f46b56909e2a110598bf0226e0d8ce856
Ryan Pepper <r.a.pepper@soton.ac.uk>
2016-01-25 18:08:44 +0000

    updated provision script

--- VagrantTemplate/provision_script.sh
+++ VagrantTemplate/provision_script.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-sudo apt-get update                                                                                                                    │==> default: Building dependency tree...
-sudo apt-get upgrade                                                                                                                   │==> default: Reading state information...
-#sudo apt-get install build-essential, python, git                                                                                     │==> default: The following packages have been kept back:
-sudo apt-get install tcl, tk, tcl8.6-dev, tk8.6-dev                                                                                   │==> default:   linux-headers-generic linux-headers-virtual linux-image-virtual
-git clone https://github.com/fangohr/oommf.git                                                                                         │==> default:   linux-virtual
-git clone https://github.com/fangohr/oommf-python.git                                                                                  │==> default: The following packages will be upgraded:
-cd oommf                                                                                                                              │==> default:   bind9-host cloud-init coreutils dnsutils gcc-4.9-base grub-common grub-pc
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y build-essential
+sudo apt-get install -y git -y  
+sudo apt-get install -y tcl tk tcl8.6-dev tk8.6-dev
+git clone https://github.com/fangohr/oommf.git 
+git clone https://github.com/fangohr/oommf-python.git
+cd oommf 
 make

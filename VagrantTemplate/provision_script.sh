81f37d6d405ceec70b6c1d51ea21c06546a154de
Ryan Pepper <r.a.pepper@soton.ac.uk>
2016-01-26 00:13:27 +0000

    Complete vagrant script which sets up a Ubuntu VM and installs OOMMF

--- VagrantTemplate/provision_script.sh
+++ VagrantTemplate/provision_script.sh
@@ -2,10 +2,12 @@
 
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install -y build-essential
-sudo apt-get install -y git -y  
-sudo apt-get install -y tcl tk tcl8.6-dev tk8.6-dev
+sudo apt-get install -y --force-yes build-essential
+sudo apt-get install -y --force-yes git 
+sudo apt-get install -y --force-yes tcl tk tcl8.6-dev tk8.6-dev
 git clone https://github.com/fangohr/oommf.git 
 git clone https://github.com/fangohr/oommf-python.git
 cd oommf 
+export OOMMF_TK_CONFIG=/usr/lib/x86_64-linux-gnu/tk8.6/tkConfig.sh
+export OOMMF_TCL_CONFIG=/usr/lib/x86_64-linux-gnu/tcl8.6/tclConfig.sh
 make

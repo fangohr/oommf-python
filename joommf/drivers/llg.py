d49e370744fc18666cba109bab084c0f0736e080
Ryan Pepper <r.a.pepper@soton.ac.uk>
2016-02-05 16:04:56 +0000

    PEP8 llg.py

--- joommf/drivers/llg.py
+++ joommf/drivers/llg.py
@@ -1,10 +1,12 @@
 import textwrap
 
+
 class LLG(object):
 
-    def __init__(self, t, m_init, Ms, alpha, gamma, name, solver='rkf54', dm=0.01):
+    def __init__(self, t, m_init, Ms, alpha, gamma,
+                 name, solver='rkf54', dm=0.01):
         """
-        Note: 
+        Note:
         solver options passed as a string - options
         rk2, rk4, rkf54, rkf54m, rkf54s
 
@@ -25,7 +27,7 @@ class LLG(object):
                        gamma_G {:.5f}
                        start_dm {:.5f}
                    }}
-                   
+
                        Specify Oxs_TimeDriver [subst {{
                        evolver :evolve
                        stopping_time {:.2e}
@@ -38,22 +40,22 @@ class LLG(object):
                        basename {}
                    vector_field_output_format {{text \%#.8g}}
                    }}]
-                   
-                   
+
+
                    """)
-                   
+
         return llg_mif.format(self.solver,
-                             self.alpha,
-                             self.gamma,
-                             self.dm,
-                             self.t,
-                             self.Ms,
-                             self.m_init[0],
-                             self.m_init[1],
-                             self.m_init[2],
-                             self.name
-                             )
-        
+                              self.alpha,
+                              self.gamma,
+                              self.dm,
+                              self.t,
+                              self.Ms,
+                              self.m_init[0],
+                              self.m_init[1],
+                              self.m_init[2],
+                              self.name
+                              )
+
 if __name__ == '__main__':
     llg = LLG(1e-9, (0, 0, 1), 1e6, 0.1, 2.21e5, 'test')
     f = open('test_llg.mif', 'w')

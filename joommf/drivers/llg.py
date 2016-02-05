e82cee6801b337a0d2d409fb1e3fc4cf455aded3
Ryan Pepper <r.a.pepper@soton.ac.uk>
2016-02-05 15:37:37 +0000

    Change to textwrapped triple quoted strings for easier mif changes in llg.py

--- joommf/drivers/llg.py
+++ joommf/drivers/llg.py
@@ -1,6 +1,8 @@
+import textwrap
+
 class LLG(object):
 
-    def __init__(self, t, m_init, Ms, alpha, gamma, name, solver='rkf54'):
+    def __init__(self, t, m_init, Ms, alpha, gamma, name, solver='rkf54', dm=0.01):
         """
         Note: 
         solver options passed as a string - options
@@ -14,30 +16,44 @@ class LLG(object):
         self.gamma = gamma
         self.name = name
         self.solver = solver
+        self.dm = dm
 
     def get_mif(self):
-        llg_mif = 'Specify Oxs_RungeKuttaEvolve:evolve {\n'
-        llg_mif += '\tmethod ${}'.format(self.solver)
-        llg_mif += '\talpha %.5f\n' % self.alpha
-        llg_mif += '\tgamma_G %.5f\n' % self.gamma
-        llg_mif += '\tstart_dm 0.01\n'
-        llg_mif += '}\n\n'
-
-        llg_mif += 'Specify Oxs_TimeDriver [subst {\n'
-        llg_mif += '\tevolver :evolve\n'
-        llg_mif += '\tstopping_time %2e\n' % self.t
-        llg_mif += '\tstage_count 1\n'
-        llg_mif += '\tmesh :mesh\n'
-        llg_mif += '\tMs %2e\n' % self.Ms
-        llg_mif += '\tm0 { Oxs_UniformVectorField {\n'
-        llg_mif += '\t\tvector {%.5f %.5f %.5f}\n' % (
-            self.m_init[0], self.m_init[1], self.m_init[2])
-        llg_mif += '\t} }\n'
-        llg_mif += '\tbasename ' + self.name + ' \n'
-        llg_mif += 'vector_field_output_format {text \%#.8g}\n'
-        llg_mif += '}]\n\n'
-        return llg_mif
-
+        llg_mif = textwrap.dedent("""\
+                   Specify Oxs_RungeKuttaEvolve:evolve {{
+                       method ${}   alpha {:.5f}
+                       gamma_G {:.5f}
+                       start_dm {:.5f}
+                   }}
+                   
+                       Specify Oxs_TimeDriver [subst {{
+                       evolver :evolve
+                       stopping_time {:.2e}
+                       stage_count 1
+                       mesh :mesh
+                       Ms {:.5e}
+                       m0 {{ Oxs_UniformVectorField {{
+                           vector {{{:.5f}, {:.5f}, {:.5f}}}
+                       }} }}
+                       basename {}
+                   vector_field_output_format {{text \%#.8g}}
+                   }}]
+                   
+                   
+                   """)
+                   
+        return llg_mif.format(self.solver,
+                             self.alpha,
+                             self.gamma,
+                             self.dm,
+                             self.t,
+                             self.Ms,
+                             self.m_init[0],
+                             self.m_init[1],
+                             self.m_init[2],
+                             self.name
+                             )
+        
 if __name__ == '__main__':
     llg = LLG(1e-9, (0, 0, 1), 1e6, 0.1, 2.21e5, 'test')
     f = open('test_llg.mif', 'w')

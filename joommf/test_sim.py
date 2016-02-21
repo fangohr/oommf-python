22cdba24db55cb0bf432f603555bd088714f2d50
Ryan Pepper <r.a.pepper@soton.ac.uk>
2016-02-21 22:04:20 +0000

    Cover add_energy fn in test_sim.py

--- joommf/test_sim.py
+++ joommf/test_sim.py
@@ -1,7 +1,8 @@
 import mesh
 import sim
-
+from energies.demag import Demag
 
 def test_sim_basic_square():
     testmesh = mesh.Mesh((10, 20, 30), (3, 3, 3), scale=1e-9)
     sim.Sim(testmesh, 1e4)
+    sim.add_energy(Demag())

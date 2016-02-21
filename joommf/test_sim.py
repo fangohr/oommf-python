ace8c53e6cbb32d6cf1fe72939f1a6e7ad89e9ff
Ryan Pepper <r.a.pepper@soton.ac.uk>
2016-02-21 22:17:13 +0000

    Assign sim to variable in test_sim.py

--- joommf/test_sim.py
+++ joommf/test_sim.py
@@ -4,5 +4,5 @@ from energies.demag import Demag
 
 def test_sim_basic_square():
     testmesh = mesh.Mesh((10, 20, 30), (3, 3, 3), scale=1e-9)
-    sim.Sim(testmesh, 1e4)
-    sim.add_energy(Demag())
+    a = sim.Sim(testmesh, 1e4)
+    a.add_energy(Demag())

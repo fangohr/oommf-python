827ad37a606a9203abafecd73ad61e8acc18bbb7
ryanpepper <ryan.pepper1@gmail.com>
2016-03-07 17:09:17 +0000

    Define JoommfError exception class; Add add_output(), add_evolver() and __repr__() to Sim class

--- joommf/sim.py
+++ joommf/sim.py
@@ -1,9 +1,100 @@
-from subprocess import Popen, PIPE, STDOUT
 import os
 from drivers.evolver import LLG
 from drivers.evolver import Minimiser
 
 import oommfmif as o
+
+"Soon to be supported outputs"
+
+time_evolver_outputs = ['time', 'Iteration', 'Stage iteration', 'Stage',
+                        'Last time step', 'Simulation time',
+                        'mx', 'my', 'mz',
+                        'Magnetization', 'Spin']
+
+minimizer_outputs = ['H', 'Total energy density', 'mxHxm',
+                     'Magnetization', 'Spin']
+
+field_outputs = ['UniformExchange', 'Demag', 'FixedZeeman', 'UZeeman',
+                 'UniaxialAnisotropy', 'CubicAnisotropy', 'ExchangePtwise',
+                 'Exchange6Ngbr']
+
+
+"""
+Outputs from OOMMF:
+Energies:
+Oxs_UniformExchange::Energy density
+Oxs_UniformExchange::Field
+Oxs_UniformExchange::Max Spin Ang
+Oxs_UniformExchange::Stage Max Spin Ang
+Oxs_UniformExchange::Run Max Spin Ang
+Oxs_UniformExchange::Energy
+
+Oxs_Demag::Energy density
+Oxs_Demag::Field
+Oxs_Demag::Energy
+
+Oxs_FixedZeeman::Energy density
+Oxs_FixedZeeman::Field
+Oxs_FixedZeeman::Energy
+
+Oxs_UniaxialAnisotropy::Field
+Oxs_UniaxialAnisotropy::Energy density
+Oxs_UniaxialAnisotropy::Energy
+
+Oxs_CubicAnisotropy::Energy density
+Oxs_CubicAnisotropy::Field
+Oxs_CubicAnisotropy::Energy
+
+Oxs_ExchangePtwise::Energy density
+Oxs_ExchangePtwise::Field
+Oxs_ExchangePtwise::Energy
+
+Oxs_UZeeman::Energy density
+Oxs_UZeeman::Field
+Oxs_UZeeman::Energy
+Oxs_UZeeman::B
+Oxs_UZeeman::Bx
+Oxs_UZeeman::By
+Oxs_UZeeman::Bz
+
+Oxs_Exchange6Ngbr:Field
+Oxs_Exchange6Ngbr:Energy density
+Oxs_Exchange6Ngbr:Energy
+
+Time Evolvers:
+Oxs RungeKuttaEvolve:evolver:Total energy
+Oxs_RungeKuttaEvolve:evolver:Energy calc count
+Oxs_RungeKuttaEvolve:evolver:Max dm/dt
+Oxs_RungeKuttaEvolve:evolver:dE/dt
+Oxs_RungeKuttaEvolve:evolver:Delta E
+Oxs_RungeKuttaEvolve:evolver:Total energy density
+Oxs_RungeKuttaEvolve:evolver:Total field
+Oxs_RungeKuttaEvolve:evolver:dm/dt
+Oxs_RungeKuttaEvolve:evolver:mxH
+
+Oxs_TimeDriver::Iteration
+Oxs_TimeDriver::Stage iteration
+Oxs_TimeDriver::Stage
+Oxs_TimeDriver::Magnetization
+Oxs_TimeDriver::Spin
+Oxs_TimeDriver::Last time step
+Oxs_TimeDriver::Simulation time
+Oxs_TimeDriver::mx
+Oxs_TimeDriver::my
+Oxs_TimeDriver::mz
+
+Minimisation Evolvers:
+Oxs_CGEvolve::H
+Oxs_CGEvolve::Total energy density
+Oxs_CGEvolve::mxHxm
+Oxs_MinDriver::Magnetization
+Oxs_MinDriver::Spin
+
+"""
+
+
+class JoommfError(Exception):
+    pass
 
 
 class Sim(object):
@@ -17,24 +108,53 @@ class Sim(object):
         self.N_Sims_Run = 0
         self._oommf_stdout = ''
         self._oommf_stderr = ''
-        # Want some kind of persistent 'knowledge' of number of runs
-        # and the sequence these occur in for data analysis
-        # when we call a simulation multiple times to either
-        # advance time or change parameters. Will need to think carefully
-        # about situations such as changing H_applied - should we recreate this
-        # data from the output files?
-        # Advantage of this is recreating sim object if needed.
+        self.field_outputs = []
+        self.evolver_outputs = []
+        self.evolver = None
+
+    def __repr__(self):
+        string = "Joommf Sim Object - Mesh: {}\n Ms: {}\n gamma: {}\n".format(
+                 self.mesh.__repr__(), self.Ms, self.gamma)
+        if self.evolver:
+            string += "Evolver: {}".format(self.evolver.__repr__())
+        return string
 
     def add_energy(self, energy):
         self.energies.append(energy)
 
+    def add_evolver(self, evolver):
+        self.evolver = evolver
+
+    def add_output(self, output, freq):
+        if not self.evolver:
+            raise JoommfError("Joommf: You must add an evolver before "
+                              "scheduling outputs, as some evolvers do "
+                              "not support certain outputs.")
+        if isinstance(self.evolver, LLG):
+            if output in time_evolver_outputs:
+                self.outputs.append(output)
+            elif output in minimizer_outputs:
+                raise JoommfError("Joommf: This output is not supported by"
+                                  " time integrator evolvers.")
+        elif isinstance(self.evolver, Minimiser):
+            if output in minimizer_outputs:
+                self.outputs.append(output)
+            elif output in time_evolver_outputs:
+                raise JoommfError("Joommf: This output is not supported by"
+                                  " minimization evolvers.")
+        elif output in field_outputs:
+            self.evolver_outputs.append(output)
+        else:
+            raise JoommfError("Joommf: This output was not understood."
+                              " Please check that it is supported.")
+
     def set_m(self, m_init):
         self.m_init = m_init
 
-    def create_mif(self, evolver, overwrite=True):
+    def create_mif(self, overwrite=True):
         if self.name is None:
             self.name = 'unnamed'
-        self.mif_filename = self.name + '_iteration' + \
+        self.mif_filename = self.name + \
             str(self.N_Sims_Run) + '.mif'
         if os.path.isfile(self.mif_filename):
             print("DEBUG: This simulation name already exists.")
@@ -45,18 +165,18 @@ class Sim(object):
         mif_file.write(self.mesh.mesh_mif())
         for energy in self.energies:
             mif_file.write(energy.get_mif())
-        mif_file.write(evolver.get_mif())
+        mif_file.write(self.evolver.get_mif())
         mif_file.close()
 
     def run_until(self, t, alpha=0.1, gamma=2.21e5):
-        llg = LLG(t, self.m_init, self.Ms, alpha, gamma, name=self.name)
-        self.create_mif(llg)
-        self.execute_mif()
+        if isinstance(self.evolver, LLG):
+            self.create_mif(self.evolver)
+            self.execute_mif()
 
     def minimise(self):
-        minim = Minimiser(self.m_init, self.Ms, name=self.name)
-        self.create_mif(minim)
-        self.execute_mif()
+        if isinstance(self.evolver, Minimiser):
+            self.create_mif(self.evolver)
+            self.execute_mif()
 
     def execute_mif(self):
         # path = o.retrieve_oommf_path()

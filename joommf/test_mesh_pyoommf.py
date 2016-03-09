c9a5066f856d05229e0de7df4428cfd6fa4c2554
ryanpepper <ryan.pepper1@gmail.com>
2016-03-09 13:32:39 +0000

    Fix test_mesh_pyoommf

--- joommf/test_mesh_pyoommf.py
+++ joommf/test_mesh_pyoommf.py
@@ -1,6 +1,6 @@
-import mesh
+import joommf.mesh
 
 
 def test_Mesh():
-    a = mesh.Mesh((10, 20, 30), (1, 2, 3))
+    a = joommf.mesh.Mesh((10, 20, 30), (1, 2, 3))
     assert a.mesh_info() == (10, 20, 30, 1, 2, 3)

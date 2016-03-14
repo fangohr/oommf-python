9115628cf10e194f1975e01142d8ae08ab5c4b06
ryanpepper <ryan.pepper1@gmail.com>
2016-03-14 17:36:12 +0000

    Add test for pandas dataframe loading

---
+++ joommf/test_odtreader.py
@@ -0,0 +1,36 @@
+def test_odtreader_dynamics_example():
+    from joommf.sim import Sim
+    from joommf.mesh import Mesh
+    from joommf.energies.exchange import Exchange
+    from joommf.energies.demag import Demag
+    from joommf.energies.zeeman import FixedZeeman
+    from joommf.drivers import evolver
+    # Mesh specification.
+    lx = ly = lz = 50e-9  # x, y, and z dimensions (m)
+    dx = dy = dz = 5e-9  # x, y, and z cell dimensions (m)
+
+    Ms = 8e5  # saturation magnetisation (A/m)
+    A = 1e-11  # exchange energy constant (J/m)
+    H = (1e3, 0, 0)  # external magnetic field (A/m)
+    m_init = (0, 0, 1)  # initial magnetisation
+    t_sim = 0.5e-9  # simulation time (s)
+    gamma = 2.21e5
+    alpha = 0.1
+    # Create a mesh.
+    mesh = Mesh((lx, ly, lz), (dx, dy, dz))
+
+    # Create a simulation object.
+    sim = Sim(mesh, Ms, name='dynamics_example', debug=True)
+
+    # Add energies.
+    sim.add_energy(Exchange(A))
+    sim.add_energy(Demag())
+    sim.add_energy(FixedZeeman(H))
+    sim.set_evolver(
+        evolver.LLG(t_sim, m_init, Ms, alpha, gamma, name='evolver'))
+    # Set initial magnetisation.
+    sim.set_m(m_init)
+    # Run simulation.
+    sim.run()
+
+    assert sim.df.time.values[-1] == 0.5e-09

09c831b96e54a85bad6a978dcd1719150f2cc065
Hans Fangohr <fangohr@soton.ac.uk>
2016-03-18 15:38:32 +0000

    make relative issue work for python 3

--- joommf/test_examples.py
+++ joommf/test_examples.py
@@ -1,8 +1,17 @@
+import os
+import sys
+
+# putting 'joommf' into search path
+sys.path.insert(0, os.path.abspath('..'))
+print(sys.path)
+
+
 def test_run_min_example():
-    import minimisation_example
-    minimisation_example.main()
+    import joommf.minimisation_example
+    joommf.minimisation_example.main()
+
 
 
 def test_run_dyn_example():
-    import dynamics_example
-    dynamics_example.main()
+    import joommf.dynamics_example
+    joommf.dynamics_example.main()

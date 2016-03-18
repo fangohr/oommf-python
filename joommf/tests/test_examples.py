c67678a52539144a47b2af91d88aed67085ab2ee
ryanpepper <ryan.pepper1@gmail.com>
2016-03-18 14:57:01 +0000

    Fix test_examples for Python 3

--- joommf/tests/test_examples.py
+++ joommf/tests/test_examples.py
@@ -1,8 +1,8 @@
 def run_min_example():
-    import minimisation_example
-    minimisation_example.main()
+    import joommf.minimisation_example
+    joommf.minimisation_example.main()
 
 
 def run_dyn_example():
-    import dynamics_example
-    dynamics_example.main()
+    import joommf.dynamics_example
+    joommf.dynamics_example.main()

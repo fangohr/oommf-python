03d3abf90d543e75963c728f87ceddb253b6324d
mb4e10 <mb4e10@soton.ac.uk>
2016-04-21 17:43:50 +0100

    Add Field class (will be used for reading/saving ovf files, plotting, and analysis).

---
+++ joommf/field.py
@@ -0,0 +1,217 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Field(object):
+    def __init__(self, cmin, cmax, d, dim=1, value=None, name=None):
+        self.cmin = cmin
+        self.cmax = cmax
+        self.d = d
+        self.dim = dim
+        self.name = name
+
+        # Compute domain edge lengths.
+        self.l = (self.cmax[0]-self.cmin[0],
+                  self.cmax[1]-self.cmin[1],
+                  self.cmax[2]-self.cmin[2])
+
+        # Compute the number of cells in x, y, and z directions.
+        tol = 1e-12
+        if ((self.d[0] - tol > self.l[0] % self.d[0] > tol) or \
+            (self.d[1] - tol > self.l[1] % self.d[1] > tol) or \
+            (self.d[2] - tol > self.l[2] % self.d[2] > tol)):
+            raise ValueError('Domain is not a multiple of {}.'.format(self.d))
+
+        else:
+            self.n = (int(round(self.l[0]/self.d[0])),
+                      int(round(self.l[1]/self.d[1])),
+                      int(round(self.l[2]/self.d[2])))
+
+        # Create an empty 3d vector field.
+        self.f = np.zeros([self.n[0], self.n[1], self.n[2], dim])
+
+        if value is not None:
+            self.set(value)
+
+    def __call__(self, c):
+        return self.sample(c)
+
+    def domain_centre(self):
+        c = (self.cmin[0] + 0.5*self.l[0],
+             self.cmin[1] + 0.5*self.l[1],
+             self.cmin[2] + 0.5*self.l[2])
+
+        return c
+
+    def random_coord(self):
+        c = (self.cmin[0] + random.random()*self.l[0],
+             self.cmin[1] + random.random()*self.l[1],
+             self.cmin[2] + random.random()*self.l[2])
+
+        return c
+
+    def index2coord(self, i):
+        if i[0] < 0 or i[0] > self.n[0]-1 or \
+           i[1] < 0 or i[1] > self.n[1]-1 or \
+           i[2] < 0 or i[2] > self.n[2]-1:
+            raise ValueError('Index {} out of range.'.format(i))
+
+        else:
+            c = (self.cmin[0] + (i[0] + 0.5)*self.d[0],
+                 self.cmin[1] + (i[1] + 0.5)*self.d[1],
+                 self.cmin[2] + (i[2] + 0.5)*self.d[2])
+
+        return c
+
+    def coord2index(self, c):
+        if (c[0] < self.cmin[0] or c[0] > self.cmax[0] or \
+            c[1] < self.cmin[1] or c[1] > self.cmax[1] or \
+            c[2] < self.cmin[2] or c[2] > self.cmax[2]):
+            raise ValueError('Coordinate {} out of domain.'. format(c))
+
+        else:
+           i = [int(round(float(c[0]-self.cmin[0])/self.d[0] - 0.5)),
+                int(round(float(c[1]-self.cmin[1])/self.d[1] - 0.5)),
+                int(round(float(c[2]-self.cmin[2])/self.d[2] - 0.5))]
+
+           # If rounded to the out-of-range index.
+           for j in range(3):
+               if i[j] < 0:
+                   i[j] = 0
+               elif i[j] > self.n[j] - 1:
+                   i[j] = self.n[j] - 1
+
+        return tuple(i)
+
+    def nearestcellcoord(self, c):
+        return self.index2coord(self.coord2index(c))
+
+    def sample(self, c):
+        i = self.coord2index(c)
+        return self.f[i[0], i[1], i[2]]
+
+    def set(self, value, normalise=False):
+        if isinstance(value, (int, float)):
+            self.f.fill(value)
+
+        elif isinstance(value, (tuple, list, np.ndarray)):
+            for i in range(self.dim):
+                self.f[:, :, :, i].fill(value[i])
+
+        elif hasattr(value, '__call__'):
+            for ix in xrange(self.n[0]):
+                for iy in xrange(self.n[1]):
+                    for iz in xrange(self.n[2]):
+                        i = (ix, iy, iz)
+                        coord = self.index2coord((ix, iy, iz))
+                        self.f[ix, iy, iz, :] = value(coord)
+
+        else:
+            raise TypeError("Cannot set field using {}.".format(type(value)))
+
+        if normalise:
+            self.normalise()
+
+    def slice_field(self, axis, point):
+        if axis == 'x':
+            slice_num = 0
+            axes = (1, 2)
+        elif axis == 'y':
+            slice_num = 1
+            axes = (0, 2)
+        elif axis == 'z':
+            slice_num = 2
+            axes = (0, 1)
+        else:
+            raise ValueError('Axis not properly defined.')
+        
+
+        if self.cmin[slice_num] <= point <= self.cmax[slice_num]:
+            axis1_indices = np.arange(0, self.n[axes[0]])
+            axis2_indices = np.arange(0, self.n[axes[1]])
+
+            axis1_coords = np.zeros(len(axis1_indices))
+            axis2_coords = np.zeros(len(axis2_indices))
+            
+            sample_centre = list(self.domain_centre())
+            sample_centre[slice_num] = point
+            sample_centre = tuple(sample_centre)
+
+            slice_index = self.coord2index(sample_centre)[slice_num]
+
+            field_slice = np.zeros([self.n[axes[0]], self.n[axes[1]], self.dim])
+            for j in axis1_indices:
+                for k in axis2_indices:
+                    i = [0, 0, 0]
+                    i[slice_num] = slice_index
+                    i[axes[0]] = j
+                    i[axes[1]] = k
+                    i = tuple(i)
+
+                    coord = self.index2coord(i)
+
+                    axis1_coords[j] = coord[axes[0]]
+                    axis2_coords[k] = coord[axes[1]]
+
+                    field_slice[j, k, :] = self.f[i[0], i[1], i[2], :]
+            coord_system = (axes[0], axes[1], slice_num)
+
+        else:
+            raise ValueError('Point {} outside the domain.'.format(point))
+
+        return axis1_coords, axis2_coords, field_slice, coord_system
+            
+
+    def plot_slice(self, axis, point):
+        a1, a2, field_slice, coord_system = self.slice_field(axis, point)
+        
+        if self.dim == 1:
+            plt.imshow(field_slice[:, :, 0],
+                       extent=[a1[0], a1[-1], a2[0], a2[-1]],
+                       aspect='auto')
+
+        elif self.dim == 3:
+            pm = self._prepare_for_quiver(a1, a2,
+                                          field_slice,
+                                          coord_system)
+            plt.quiver(pm[:, 0], pm[:, 1], pm[:, 2], pm[:, 3], pm[:, 4])
+        plt.xlim([self.cmin[coord_system[0]], self.cmax[coord_system[0]]])
+        plt.ylim([self.cmin[coord_system[1]], self.cmax[coord_system[1]]])
+        plt.xlabel('xyz'[coord_system[0]])
+        plt.ylabel('xyz'[coord_system[1]])
+        plt.title('xyz'[coord_system[2]] + ' slice')
+        plt.grid()
+        plt.show()
+
+    def _prepare_for_quiver(self, a1, a2, field_slice, coord_system):
+        nel = self.n[coord_system[0]]*self.n[coord_system[1]]
+        plot_matrix = np.zeros([nel, 5])
+
+        counter = 0
+        for j in xrange(self.n[coord_system[0]]):
+            for k in xrange(self.n[coord_system[1]]):
+                entry = [a1[j], a2[k],
+                         field_slice[j, k, 0],
+                         field_slice[j, k, 1],
+                         field_slice[j, k, 2]]
+                plot_matrix[counter, :] = np.array(entry)
+                counter += 1
+
+        return plot_matrix
+
+    def normalise(self, norm=1):
+        if self.dim == 1:
+            raise NotImplementedError("""Normalisation of scalar
+                                      fields is not implemented.""")
+
+        else:
+            # Compute norm.
+            f_norm = 0
+            for j in range(self.dim):
+                f_norm += self.f[:, :, :, j]**2
+            f_norm = np.sqrt(f_norm)
+
+            for j in range(self.dim):
+                self.f[:, :, :, j] = norm * self.f[:, :, :, j]/f_norm
+                 
---
+++ joommf/field_test.py
@@ -0,0 +1,239 @@
+import random
+import numpy as np
+from field import Field
+
+class TestField(object):
+    def setup(self):
+        self.scalar_fs = self.create_scalar_fs()
+        self.vector_fs = self.create_vector_fs()
+        self.constant_values = [0, -5., np.pi,
+                                1e-15, 1.2e12, random.random()]
+        self.tuple_values = [(0, 0, 1),
+                             (0, -5.1, np.pi),
+                             [70, 1e15, 2*np.pi],
+                             [5, random.random(), np.pi],
+                             np.array([4, -1, 3.7]),
+                             np.array([2.1, 0.0, -5*random.random()])]
+        self.scalar_pyfuncs = self.create_scalar_pyfuncs()
+        self.vector_pyfuncs = self.create_vector_pyfuncs()
+
+    def create_scalar_fs(self):
+        cmin_list = [(0, 0, 0), (-5, -8, -10), (10, -5, -80)]
+        cmax_list = [(5, 8, 10), (11, 4, 4), (15, 10, 85)]
+        d_list = [(1, 1, 1), (1, 2, 1), (5, 5, 2.5)]
+
+        scalar_fs = []
+        for i in range(len(cmin_list)):
+            f = Field(cmin_list[i], cmax_list[i], d_list[i], 1)
+            scalar_fs.append(f)
+
+        return scalar_fs
+
+    def create_vector_fs(self):
+        cmin_list = [(0, 0, 0), (-5, -8, -10), (10, -5, -80)]
+        cmax_list = [(5, 8, 10), (11, 4, 4), (15, 10, 85)]
+        d_list = [(1, 1, 1), (1, 2, 1), (5, 5, 2.5)]
+
+        vector_fs = []
+        for i in range(len(cmin_list)):
+            f = Field(cmin_list[i], cmax_list[i], d_list[i], 3)
+            vector_fs.append(f)
+
+        return vector_fs
+
+    def create_scalar_pyfuncs(self):
+        f1 = lambda c: 1
+        f2 = lambda c: -2.4
+        f3 = lambda c: -6.4e-15
+        f4 = lambda c: c[0] + c[1] + c[2] + 1
+        f5 = lambda c: (c[0]-1)**2 - c[1]+7 + c[2]*0.1
+        f6 = lambda c: np.sin(c[0]) + np.cos(c[1]) - np.sin(2*c[2])
+
+        return [f1, f2, f3, f4, f5, f6]
+
+    def create_vector_pyfuncs(self):
+        f1 = lambda c: (1, 2, 0)
+        f2 = lambda c: (-2.4, 1e-3, 9)
+        f3 = lambda c: (c[0], c[1], c[2] + 100)
+        f4 = lambda c: (c[0]+c[2]+10, c[1], c[2]+1)
+        f5 = lambda c: (c[0]-1, c[1]+70, c[2]*0.1)
+        f6 = lambda c: (np.sin(c[0]), np.cos(c[1]), -np.sin(2*c[2]))
+
+        return [f1, f2, f3, f4, f5, f6]
+
+    def test_init(self):
+        cmin = (0, -4, 11)
+        cmax = (15, 10.1, 16.5)
+        d = (1, 0.1, 0.5)
+        name = 'test_field'
+
+        f = Field(cmin, cmax, d, dim=2, name=name)
+    
+        assert f.l[0] == 15 - 0
+        assert f.l[1] == 10.1 - (-4)
+        assert f.l[2] == 16.5 - 11
+
+        assert f.n[0] == (15 - 0) / 1.0
+        assert f.n[1] == (10.1 - (-4)) / 0.1
+        assert f.n[2] == (16.5 - 11) / 0.5
+
+        assert type(f.n[0]) is int
+        assert type(f.n[1]) is int
+        assert type(f.n[2]) is int
+
+        assert f.name == name
+
+        assert np.all(f.f == 0)
+
+    def test_index2coord(self):
+        cmin = (-1, -4, 11)
+        cmax = (15, 10.1, 12.5)
+        d = (1, 0.1, 0.5)
+        name = 'test_field'
+
+        f = Field(cmin, cmax, d, dim=2, name=name)
+
+        assert f.index2coord((0, 0, 0)) == (-0.5, -3.95, 11.25)
+        assert f.index2coord((5, 10, 1)) == (4.5, -2.95, 11.75)
+
+    def test_coord2index(self):
+        cmin = (-10, -5, 0)
+        cmax = (10, 5, 10)
+        d = (1, 5, 1)
+        name = 'test_field'
+
+        f = Field(cmin, cmax, d, dim=2, name=name)
+
+        assert f.coord2index((-10, -5, 0)) == (0, 0, 0)
+        assert f.n[0] == 20
+        assert f.coord2index((10, 5, 10)) == (19, 1, 9)
+        assert f.coord2index((0.0001, 0.0001, 5.0001)) == (10, 1, 5)
+        assert f.coord2index((-0.0001, -0.0001, 4.9999)) == (9, 0, 4)
+
+    def test_domain_centre(self):
+        cmin = (-18.5, 5, 0)
+        cmax = (10, 10, 10)
+        d = (0.1, 0.25, 2)
+        name = 'test_field'
+
+        f = Field(cmin, cmax, d, dim=2, name=name)
+
+        assert f.domain_centre() == (-4.25, 7.5, 5)
+
+    def test_random_coord(self):
+        cmin = (-18.5, 5, 0)
+        cmax = (10, 10, 10)
+        d = (0.1, 0.25, 2)
+        name = 'test_field'
+
+        f = Field(cmin, cmax, d, dim=2, name=name)
+
+        for j in range(500):
+            c = f.random_coord()
+            assert f.cmin[0] <= c[0] <= f.cmax[0]
+            assert f.cmin[1] <= c[1] <= f.cmax[1]
+            assert f.cmin[2] <= c[2] <= f.cmax[2]
+
+    def test_set_with_constant(self):
+        for value in self.constant_values:
+            for f in self.scalar_fs + self.vector_fs:
+                f.set(value)
+
+                # Check all values.
+                assert np.all(f.f == value)
+
+                # Check with sampling.
+                assert np.all(f(f.random_coord()) == value)
+                assert np.all(f.sample(f.random_coord()) == value)
+
+    def test_set_with_tuple_list_ndarray(self):
+        for value in self.tuple_values:
+            for f in self.vector_fs:
+                f.set(value)
+
+                for j in range(3):
+                    assert np.all(f.f[:, :, :, j] == value[j])
+                    assert np.all(f(f.random_coord())[j] == value[j])
+                    assert np.all(f.sample(f.random_coord())[j] == value[j])
+
+    def test_set_from_callable(self):
+        # Test scalar fs.
+        for f in self.scalar_fs:
+            for pyfun in self.scalar_pyfuncs:
+                f.set(pyfun)
+                
+                for j in range(10):
+                    c = f.random_coord()
+                    expected_value = pyfun(f.nearestcellcoord(c))
+                    assert f(c) == expected_value
+                    assert f.sample(c) == expected_value
+
+        # Test vector fields.
+        for f in self.vector_fs:
+            for pyfun in self.vector_pyfuncs:
+                f.set(pyfun)
+
+                for j in range(10):
+                    c = f.random_coord()
+                    expected_value = pyfun(f.nearestcellcoord(c))
+                    assert np.all(f(c) == expected_value)
+                    assert np.all(f.sample(c) == expected_value)
+                    
+    def test_slice_f(self):
+        for s in 'xyz':
+            for f in self.vector_fs + self.scalar_fs:
+                if f.dim == 1:
+                    funcs = self.scalar_pyfuncs
+                elif f.dim == 3:
+                    funcs = self.vector_pyfuncs
+
+                for pyfun in funcs:
+                    f.set(pyfun)
+                    point = f.domain_centre()['xyz'.find(s)]
+                    data = f.slice_field(s, point)
+                    a1, a2, f_slice, cs = data
+                    
+                    if s == 'x':
+                        assert cs == (1, 2, 0)
+                    elif s == 'y':
+                        assert cs == (0, 2, 1)
+                    elif s == 'z':
+                        assert cs == (0, 1, 2)
+
+                    assert a1[0] == f.cmin[cs[0]] + f.d[cs[0]]/2.
+                    assert a1[-1] == f.cmax[cs[0]] - f.d[cs[0]]/2.
+                    assert a2[0] == f.cmin[cs[1]] + f.d[cs[1]]/2.
+                    assert a2[-1] == f.cmax[cs[1]] - f.d[cs[1]]/2.
+                    assert len(a1) == f.n[cs[0]]
+                    assert len(a2) == f.n[cs[1]]
+                    assert f_slice.shape == (f.n[cs[0]],
+                                                 f.n[cs[1]],
+                                                 f.dim)
+
+                    for j in xrange(f.n[cs[0]]):
+                        for k in xrange(f.n[cs[1]]):
+                            c = list(f.domain_centre())
+                            c[cs[0]] = a1[j]
+                            c[cs[1]] = a2[k]
+                            c = tuple(c)
+
+                            assert np.all(f_slice[j, k] == f(c))
+
+    def test_normalise(self):
+        for f in self.vector_fs:
+            for value in self.vector_pyfuncs + self.tuple_values:
+                for norm_value in [1, 2.1, 50, 1e-3]:
+                    f.set(value)
+
+                    f.normalise(norm=norm_value)
+
+                    # Compute norm.
+                    norm = 0
+                    for j in range(f.dim):
+                        norm += f.f[:, :, :, j]**2
+                    norm = np.sqrt(norm)
+
+                    assert norm.shape == (f.n[0], f.n[1], f.n[2])
+                    diff = norm - norm_value
+            
+                    assert np.all(abs(norm - norm_value) < 1e-12)

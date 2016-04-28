a2080ccf2d3422f0726d18a430b20f71761d1fb9
mb4e10 <mb4e10@soton.ac.uk>
2016-04-28 13:04:15 +0100

    Extend tests for evolvers.

--- new/evolvers_test.py
+++ new/evolvers_test.py
@@ -5,16 +5,17 @@ from evolvers import RungeKuttaEvolve
 class TestRungeKuttaEvolve(object):
     def setup(self):
         # Set of valid arguments.
-        self.args1 = [[1, 1, 1],
-                      [0.5, 1e5, 0.01],
-                      [0.05, 2.21e5, 5e6],
-                      [0.1, .1, 1e-2]]
+        self.args1 = [[1, 1, 1, 'rkf54'],
+                      [0.5, 1e5, 0.01, 'rk2'],
+                      [0.05, 2.21e5, 5e6, 'rk4'],
+                      [0.1, .1, 1e-2, 'rkf54m']]
 
         # Set of invalid arguments.
-        self.args2 = [[-0.1, 1, 1],
-                      [0.5, -1e5, 0.01],
-                      [0.05, 2.21e5, 'abc'],
-                      [0.1, .1, -1e-2]]
+        self.args2 = [[-0.1, 1, 1, 'bac'],
+                      [0.5, -1e5, 0.01, 'bac'],
+                      [0.05, 2.21e5, 'abc', 'rkf54'],
+                      [0.1, .1, -1e-2, 'rk2'],
+                      [0.1, 0.1, 0.1, 0.1]]
 
     def test_init(self):
         # Valid arguments.
@@ -22,8 +23,9 @@ class TestRungeKuttaEvolve(object):
             alpha = arg[0]
             gamma_G = arg[1]
             start_dm = arg[2]
+            method = arg[3]
 
-            evolver = RungeKuttaEvolve(alpha, gamma_G, start_dm)
+            evolver = RungeKuttaEvolve(alpha, gamma_G, start_dm, method)
 
             assert evolver.alpha == alpha
             assert isinstance(alpha, (int, float))
@@ -31,6 +33,8 @@ class TestRungeKuttaEvolve(object):
             assert isinstance(gamma_G, (int, float))
             assert evolver.start_dm == start_dm
             assert isinstance(start_dm, (int, float))
+            assert evolver.method == method
+            assert isinstance(method, str)
 
     def test_init_exceptions(self):
         # Invalid arguments (ValueError expected).
@@ -38,17 +42,19 @@ class TestRungeKuttaEvolve(object):
             alpha = arg[0]
             gamma_G = arg[1]
             start_dm = arg[2]
+            method = arg[3]
 
             with pytest.raises(ValueError):
-                evolver = RungeKuttaEvolve(alpha, gamma_G, start_dm)
+                evolver = RungeKuttaEvolve(alpha, gamma_G, start_dm, method)
 
     def test_get_mif(self):
         for arg in self.args1:
             alpha = arg[0]
             gamma_G = arg[1]
             start_dm = arg[2]
+            method = arg[3]
 
-            evolver = RungeKuttaEvolve(alpha, gamma_G, start_dm)
+            evolver = RungeKuttaEvolve(alpha, gamma_G, start_dm, method)
 
             mif = evolver.get_mif()
             mif_lines = evolver.get_mif().split('\n')
@@ -95,8 +101,14 @@ class TestRungeKuttaEvolve(object):
             assert l[0] == 'start_dm'
             assert l[1] == '$start_dm'
 
+            # Assert parameters lines
+            assert mif_lines[8][0] == '\t'
+            l = mif_lines[8].split()
+            assert l[0] == 'method'
+            assert l[1] == method
+
             # Assert mif end.
-            assert mif_lines[8] == '}'
+            assert mif_lines[9] == '}'
 
             # Assert new lines at the end of the string.
             assert mif[-2:] == '\n\n'

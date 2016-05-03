96ee37549d1d49e9ad2b2b392fbf1282afd7a71b
mb4e10 <mb4e10@soton.ac.uk>
2016-05-03 14:01:30 +0100

    Fix tests.

--- new/evolvers_test.py
+++ new/evolvers_test.py
@@ -65,50 +65,38 @@ class TestRungeKuttaEvolve(object):
             assert l[1] == 'RungeKutta'
             assert l[2] == 'evolver'
 
-            # Assert set statements.
-            expected_data = [('alpha', alpha),
-                             ('gamma_G', gamma_G),
-                             ('start_dm', start_dm)]
-            c = 0
-            for line in mif_lines[1:4]:
-                l = line.split()
-                assert l[0] == 'set'
-                assert l[1] == expected_data[c][0]
-                assert float(l[2]) == expected_data[c][1]
-                c += 1
-
             # Assert Specify line.
-            l = mif_lines[4].split()
+            l = mif_lines[1].split()
             assert l[0] == 'Specify'
             assert l[1].split(':')[0] == 'Oxs_RungeKuttaEvolve'
             assert l[2] == '{'
 
             # Assert parameters lines
+            assert mif_lines[2][0] == '\t'
+            l = mif_lines[2].split()
+            assert l[0] == 'alpha'
+            assert float(l[1]) == alpha
+
+            # Assert parameters lines
+            assert mif_lines[3][0] == '\t'
+            l = mif_lines[3].split()
+            assert l[0] == 'gamma_G'
+            assert float(l[1]) == gamma_G
+
+            # Assert parameters lines
+            assert mif_lines[4][0] == '\t'
+            l = mif_lines[4].split()
+            assert l[0] == 'start_dm'
+            assert float(l[1]) == start_dm
+
+            # Assert parameters lines
             assert mif_lines[5][0] == '\t'
             l = mif_lines[5].split()
-            assert l[0] == 'alpha'
-            assert l[1] == '$alpha'
-
-            # Assert parameters lines
-            assert mif_lines[6][0] == '\t'
-            l = mif_lines[6].split()
-            assert l[0] == 'gamma_G'
-            assert l[1] == '$gamma_G'
-
-            # Assert parameters lines
-            assert mif_lines[7][0] == '\t'
-            l = mif_lines[7].split()
-            assert l[0] == 'start_dm'
-            assert l[1] == '$start_dm'
-
-            # Assert parameters lines
-            assert mif_lines[8][0] == '\t'
-            l = mif_lines[8].split()
             assert l[0] == 'method'
             assert l[1] == method
 
             # Assert mif end.
-            assert mif_lines[9] == '}'
+            assert mif_lines[6] == '}'
 
             # Assert new lines at the end of the string.
             assert mif[-2:] == '\n\n'

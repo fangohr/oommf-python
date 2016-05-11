c06c737d609cf421a5416c3f0e397ee1228cb6ea
mb1a15 <m.beg@soton.ac.uk>
2016-05-11 10:13:04 +0100

    Add successive simulation running.

--- new/sim.py
+++ new/sim.py
@@ -1,9 +1,11 @@
 import os
+import glob
 from atlases import BoxAtlas
 from meshes import RectangularMesh
 from evolvers import RungeKuttaEvolve, CGEvolve
 from drivers import TimeDriver, MinDriver
-from field import Field
+from field import Field, load_oommf_file
+from energies.zeeman import FixedZeeman
 
 
 class Sim(object):
@@ -18,24 +20,43 @@ class Sim(object):
         # Set default alpha value.
         self.alpha = 1
 
+        self.dirname = self.name + '/'
+        if not os.path.exists(self.dirname):
+            os.makedirs(self.dirname)
+        self.mif_filename = self.dirname + self.name + '.mif'
+
+        self.m =  Field(self.atlas.cmin, self.atlas.cmax, self.mesh.d, dim=3)
+
+        self.t = 0
+
     def add(self, energy):
         self.energies.append(energy)
 
-    def run_until(self, stopping_time):
-        self.evolver = RungeKuttaEvolve(self.alpha)
-        self.driver = TimeDriver('evolver', stopping_time, 1, 'mesh',
-                                 self.Ms, self.m0, basename=self.name)
-        self.relaxation = False
-        self.execute_mif()
+    def set_H(self, H):
+        for energy in self.energies:
+            if isinstance(energy, FixedZeeman):
+                self.energies.remove(energy)
+        zeeman = FixedZeeman(H)
+        self.add(zeeman)
 
-    def run_multiple_stages(self, time_step, stages):
+    def run_until(self, stopping_time, stages=1):
+        self.m.write_oommf_file(self.dirname + 'm0file.omf')
+        self.m0 = 'm0file.omf'
+
         self.evolver = RungeKuttaEvolve(self.alpha)
-        self.driver = TimeDriver('evolver', time_step, stages, 'mesh',
-                                 self.Ms, self.m0, basename=self.name)
+        self.driver = TimeDriver('evolver', stopping_time/stages, stages,
+                                 'mesh', self.Ms, self.m0, basename=self.name)
         self.relaxation = False
         self.execute_mif()
+        self.t += stopping_time
+
+    def m_average(self):
+        return self.m.average()
 
     def relax(self, stopping_mxHxm=0.01):
+        self.m.write_oommf_file(self.dirname + 'm0file.omf')
+        self.m0 = 'm0file.omf'
+
         self.evolver = CGEvolve()
         self.driver = MinDriver('evolver', stopping_mxHxm, 'mesh',
                                 self.Ms, self.m0, basename=self.name)
@@ -43,17 +64,15 @@ class Sim(object):
         self.execute_mif()
 
     def set_m(self, m0):
-        if isinstance(m0, (list, tuple, str)):
-            self.m0 = m0
+        if isinstance(m0, (list, tuple)):
+            self.m.set(m0)
+        elif isinstance(m0, str):
+            self.m = load_oommf_file(m0)
         elif hasattr(m0, '__call__'):
-            m0field = Field(self.atlas.cmin, self.atlas.cmax, self.mesh.d,
-                            dim=3, value=m0)
-            m0field.normalise(norm=self.Ms)
-            m0field.write_oommf_file('m0file.omf')
-            self.m0 = 'm0file.omf'
+            self.m.set(m0)
         elif isinstance(m0, Field):
-            m0.write_oommf_file('m0file_from_field.omf')
-            self.m0 = 'm0file_from_field.omf'
+            self.m = m0
+            self.m.write_oommf_file(self.dirname + 'm0file.omf')
         else:
             raise ValueError('m0 type invalid.')
 
@@ -75,13 +94,26 @@ class Sim(object):
 
         return mif
 
-    def execute_mif(self):
-        self.mif_filename = self.name + '.mif'
+    def create_mif(self):
         miffile = open(self.mif_filename, 'w')
         miffile.write(self.get_mif())
         miffile.close()
 
+    def last_omf_file(self):
+        newest = max(glob.iglob(self.dirname+'*.omf'), key=os.path.getctime)
+        return newest
+
+    def update_self(self):
+        newest = self.last_omf_file()
+        self.m = load_oommf_file(newest)
+
+    def execute_mif(self):
+        self.create_mif()
+
         oommf_command = 'tclsh $OOMMFTCL boxsi +fg '
         oommf_command += self.mif_filename
         oommf_command += ' -exitondone 1'
+        os.system('cd ' + self.dirname)
         os.system(oommf_command)
+
+        self.update_self()
